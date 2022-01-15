4db7c17cf1435077d36ab8f295df484ba5eddbca
Enes Gökhan Özcan <enes.gokhan.ozcan@gmail.com>
2022-01-15 20:37:35 +0300

    JWT token created.

--- Management.API/Controllers/ILoginController.cs
+++ Management.API/Controllers/ILoginController.cs
@@ -1,90 +1,87 @@
-﻿//using AutoMapper;
-//using Management.DB.Entities.DataContext;
-//using Management.Model;
-//using Management.Model.User;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.Extensions.Configuration;
-//using Microsoft.IdentityModel.Tokens;
-//using System;
-//using System.IdentityModel.Tokens.Jwt;
-//using System.Linq;
-//using System.Security.Claims;
-//using System.Text;
+﻿using AutoMapper;
+using Management.DB.Entities.DataContext;
+using Management.Model;
+using Management.Model.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
 
-//namespace Management.API.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class ILoginController : ControllerBase
-//    {
-//        private readonly IMapper mapper;
-//        private IConfiguration config;
+namespace Management.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ILoginController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private IConfiguration config;
 
-//        public ILoginController(IConfiguration _config, IMapper _mapper)
-//        {
-//            config = _config;
-//            mapper = _mapper;
-//        }
+        public ILoginController(IConfiguration _config, IMapper _mapper)
+        {
+            config = _config;
+            mapper = _mapper;
+        }
 
-//        [AllowAnonymous]
-//        [HttpPost]
-//        public IActionResult Login([FromBody] UserLoginModel userLogin)
-//        {
-//            var user = Authenticate(userLogin);
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserLoginModel userLogin)
+        {
+            var user = Authenticate(userLogin);
 
-//            if (user != null)
-//            {
-//                var token = Generate(user);
-//                return Ok(token);
-//            }
+            if (user != null)
+            {
+                var token = Generate(user.Entity);
+                return Ok(token);
+            }
 
-//            return NotFound("User not found");
-//        }
+            return NotFound("User not found");
+        }
 
-//        private string Generate(UserViewModel user)
-//        {
-//            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
-//            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-//            var claims = new[]
-//            {
-//                new Claim(ClaimTypes.Name, user.Name),
-//                new Claim(ClaimTypes.Surname, user.Surname),
-//                new Claim(ClaimTypes.Email, user.Email),
-//            };
-//            var token = new JwtSecurityToken(config["Jwt:Issuer"],
-//              config["Jwt:Audience"],
-//              claims,
-//              expires: DateTime.Now.AddMinutes(15),
-//              signingCredentials: credentials);
+        private string Generate(UserViewModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Surname, user.Surname),
+                new Claim(ClaimTypes.Email, user.Email),
 
-//            return new JwtSecurityTokenHandler().WriteToken(token);
-//        }
+            };
+            claims.Add(new Claim(ClaimTypes.Role, user.IsAdmin.ToString()));
 
-//        private UserViewModel Authenticate(UserLoginModel userLogin)
-//        {
-//            General<UserViewModel> result = new();
+            var token = new JwtSecurityToken(config["Jwt:Issuer"],
+              config["Jwt:Audience"],
+              claims,
+              expires: DateTime.Now.AddMinutes(15),
+              signingCredentials: credentials);
 
-//            using (var srv = new ManagementContext())
-//            {
-//                var data = srv.User.FirstOrDefault(x => x.Email == userLogin.Email && x.Password == userLogin.Password);
-//                if (data is not null)
-//                {
-//                    result.IsSucces = true;
-//                    result.Entity = mapper.Map<UserViewModel>(data);
-//                    result.SuccesMessage = "Giriş işlemi başarıyla gerçekleşmiştir.";
-//                }
-//                return null;
-//            }
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
 
-//            //var currentUser = Management.DB.Entities.User.FirstOrDefault(o => o.Email== userLogin.Email.ToLower() && o.Password == userLogin.Password);
-//            //if (currentUser != null)
-//            //{
-//            //    return currentUser;
-//            //}
+        private General<UserViewModel> Authenticate(UserLoginModel userLogin)
+        {
+            General<UserViewModel> result = new();
 
-//            //return null;
-//        }
-//    }
-//}
+            using (var srv = new ManagementContext())
+            {
+                var data = srv.User.FirstOrDefault(x => x.Email == userLogin.Email && x.Password == userLogin.Password);
+                if (data is not null)
+                {
+                    result.IsSucces = true;
+                    result.Entity = mapper.Map<UserViewModel>(data);
+                    result.SuccesMessage = "Giriş işlemi başarıyla gerçekleşmiştir.";
+                }
+               
+            }
+            return result;
+        }
+    }
+}
